a8b1230efa6523bc2ed86abee6dd59f58f5c6437
ded_pihto@mail.uk <ded_pihto@mail.uk>
2019-08-09 14:37:13 +0300

    completed tests for order and product repositories

--- src/test/java/com/tradeservice/repository/order/OrderRepositoryTest.java
+++ src/test/java/com/tradeservice/repository/order/OrderRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.tradeservice.repository.order;
 
+import com.tradeservice.entity.Order;
+import com.tradeservice.entity.OrderItem;
 import com.tradeservice.entity.Product;
 import com.tradeservice.repository.product.ProductRepository;
 import org.junit.Assert;
@@ -7,82 +9,63 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-import java.util.stream.LongStream;
-
-import static java.util.stream.Collectors.toList;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DataJpaTest
 @AutoConfigureMockMvc
 public class OrderRepositoryTest {
 
-    @Autowired private ProductRepository     productRepository;
-
-    private MockMvc mockMvc;
+    public static final String CLIENT_NAME = "Alphabet inc.";
+    @Autowired private OrderRepository   orderRepository;
+    @Autowired private ProductRepository productRepository;
 
     @BeforeEach
-    public void before(){
+    public void before() {
         productRepository.deleteAll();
+        orderRepository.deleteAll();
     }
 
     @Test
     public void testSave() throws Exception {
-        Product newProduct1 = new Product("Товар #12315", 59.0);
-        productRepository.save(newProduct1);
-        Assert.assertNotNull(productRepository.findAll().stream().filter(p->p.equals(newProduct1)).findAny().orElse(null));
+        populateOrderAndItsProducts();
+        orderRepository.findAll();
+        Assert.assertEquals(orderRepository.findAll().get(0).getClientName(), CLIENT_NAME);
     }
 
     @Test
-    public void testFindByIds() throws Exception {
-        List<Product> productsToSave = LongStream.iterate(1, i -> ++i).limit(50)
-                .mapToObj(i -> new Product(i+"Товар #12315" + i, 59.0 + i))
-                .collect(toList());
-        productRepository.saveAll(productsToSave);
+    public void testRemove() throws Exception {
+        populateOrderAndItsProducts();
+        Order order = orderRepository.findAll().get(0);
+        orderRepository.delete(order);
+        Assert.assertEquals(0, orderRepository.findAll().size());
+    }
 
-        List<Product> productsFound = productRepository.findAllById(productsToSave.stream().map(Product::getProductId).collect(toList()));
-        Assert.assertEquals(productsFound,productsToSave);
+    @Test
+    public void testUpdate() throws Exception {
+        populateOrderAndItsProducts();
+        Order order = orderRepository.findAll().get(0);
+        String expectedClientName = "Google Inc.";
+        order.setClientName(expectedClientName);
+        orderRepository.saveAndFlush(order);
+        Assert.assertEquals(expectedClientName, orderRepository.findAll().get(0).getClientName());
     }
 
 
-
-
-
-    //    @Autowired private WebApplicationContext webApplicationContext;
-
-//    @Before
-//    public void before(){
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-//    }
-
-
-//        // request get
-//        mockMvc.perform(
-//                get("/user/login")
-//                        .param("email", email)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//                .andDo(print())
-//                .andExpect(status().is2xxSuccessful())
-//                .andExpect(jsonPath("$.message").value("user exist"));
-
-    //    @Test
-//    public void test_user_not_exist() throws Exception {
-//        // parameter
-//        String email = "test@test.com";
-//
-//        // request get
-//        mockMvc.perform(
-//                get("/user/login")
-//                        .param("email", email)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//                .andDo(print())
-//                .andExpect(status().is5xxServerError())
-//                .andExpect(jsonPath("$.message").value("user is not exist"));
-//    }
+    private void populateOrderAndItsProducts() {
+        Order newOrder1 = new Order(CLIENT_NAME, LocalDateTime.now(),"Los Angeles, California 90002");
+        Product product1 = productRepository.save(new Product("Товар #12315", 15.));
+        Product product2 = productRepository.save(new Product("Товар #12316", 16.));
+        newOrder1.setOrderItems(Set.of(
+                new OrderItem(product1,115),
+                new OrderItem(product2,116))
+        );
+        orderRepository.saveAndRefresh(newOrder1);
+    }
 
 }
